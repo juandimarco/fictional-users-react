04b19f3ea8d50d4c318a1365679e642770b6ba51
juandimarco <68037364+juandimarco@users.noreply.github.com>
2022-04-21 14:11:30 -0300

    address DOM structure adding Fragments and Portals

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
   };
 
   return (
-    <div>
+    <React.Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </React.Fragment>
   );
 };
 
